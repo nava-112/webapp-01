374038c06c02ef68c468790f677d407801d3c7f5
namle <namlh.lee@gmail.com>
2024-07-17 14:17:22 +0700

    webapp

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-FILEPATH = ".venv/todos.txt"
+FILEPATH = "/Users/namle/PycharmProjects/pythonProject-02/todos.txt"
 def get_todos():
     with open(FILEPATH, "r") as file:
         todos = file.readlines()
