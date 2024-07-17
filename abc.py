cfbb24fc750f0f5485d0b10b46a204d8d16d216b
namle <namlh.lee@gmail.com>
2024-07-17 10:57:04 +0700

    webapp

---
+++ abc.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("Hello Dear")
+st.subheader("This is my Todos App")
+st.write("To increase productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input("Enter a todo:",
+              on_change=add_todo,
+              key="new_todo")
+st.session_state
---
+++ functions.py
@@ -0,0 +1,10 @@
+FILEPATH = ".venv/todos.txt"
+def get_todos():
+    with open(FILEPATH, "r") as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg,filepath = FILEPATH):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
